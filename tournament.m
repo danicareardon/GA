71cb546de38fa88d2975b62288b0829f83c00419
Danica Reardon <danicareardon@Danicas-MacBook-Pro.local>
2018-03-29 14:49:40 -0700

    tournament selection

---
+++ tournament.m
@@ -0,0 +1,38 @@
+% TOURNAMENT.M          (Stochastic Universal Sampling)
+% based off of the implementation in the ga toolbox
+% modified by Danica Reardon d.reardon@live.com
+%
+% This function performs selection with a binary tournament.
+%
+% Syntax:  NewChrIx = tournament(FitnV, Nsel)
+%
+% Input parameters:
+%    FitnV     - Column vector containing the fitness values of the
+%                individuals in the population.
+%    Nsel      - number of individuals to be selected
+%
+% Output parameters:
+%    NewChrIx  - column vector containing the indexes of the selected
+%                individuals relative to the original population, shuffled.
+%                The new population, ready for mating, can be obtained
+%                by calculating OldChrom(NewChrIx,:).
+
+function NewChrIx = tournament(FitnV,Nsel);
+
+tournSize = 10;
+[Nind,~] = size(FitnV);
+NewChrIx = zeros(Nsel,1);
+
+for i = 1:Nsel
+    Rsel = randperm(Nind, tournSize);
+    best = Rsel(1);
+    for j = 2:tournSize
+        if FitnV(Rsel(j)) <= FitnV(best)
+            best = Rsel(j);
+        end
+    end
+    NewChrIx(i) = best;
+end
+        
+[~, shuf] = sort(rand(Nsel, 1));
+NewChrIx = NewChrIx(shuf);
